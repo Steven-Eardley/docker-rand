bbc98f5a1458079ed8ba23b25c2648ecd2fedae8
Steven Eardley <steve@eardley.xyz>
2017-06-29 21:23:12 +0100

    added print statement for random numbers

--- app/views.py
+++ app/views.py
@@ -7,4 +7,6 @@ random.seed("hello")
 # Serve a page which shows a random number sequence
 @app.route('/')
 def index():
-    return "Your random sequence is\n".format([random.randint(0, 100) for i in range(0, 10)])
+    randseq = [random.randint(0, 100) for i in range(0, 10)]
+    print(randseq)
+    return "Your random sequence is:\n{0}".format(randseq)
